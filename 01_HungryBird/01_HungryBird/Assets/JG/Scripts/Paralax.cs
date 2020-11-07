6ffd56c2bead2fd49ae4353eb986def1f1ca2608
Motasem Zakarneh <mutrz99@gmail.com>
2020-11-07 10:22:41 +0200

    Epic Journey has been completed, progressing with the HungryBird

---
+++ 01_HungryBird/01_HungryBird/Assets/JG/Scripts/Paralax.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    [SerializeField] Texture2D bg = null;
+    [SerializeField] Vector2 scrollSpeed = new Vector2(0.5f, 0);
+
+    MeshRenderer mr = null;
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        mr.material.mainTexture = bg;
+    }
+    void LateUpdate()
+    {
+        if (mr.material.mainTexture != bg)
+        {
+            mr.material.mainTexture = bg;
+        }
+        mr.material.mainTextureOffset += scrollSpeed * Time.deltaTime;
+    }
+}
